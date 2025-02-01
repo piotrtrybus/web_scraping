eddf1234bbf6bb892223de4994eba7c67b65c421
piotrtrybus <125976101+piotrtrybus@users.noreply.github.com>
2025-02-01 20:02:49 +0100

    Update web_scrape.py

--- web_scrape.py
+++ web_scrape.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+url = "https://www.datart.cz/gamepady-pro-playstation-4.html"
+
 def fetch_web_data(url):
     response = requests.get(url)
     response.raise_for_status()
@@ -15,9 +17,16 @@ def parse_html(response):
 def convert_to_df(soup):
     products =[]
     for product in soup.select("div.product-box-top-side"):
-        name = product.select_one("h3.item-title a").get_text(strip=True)
-        rating = product.select_one("span.bold").get_text(strip=True)
-        review_count = product.select_one("span.text-underline").get_text(strip=True)
+
+        name_raw = product.select_one("h3.item-title a").get_text(strip=True)
+        name = name_raw if name_raw else "Unknown"
+
+        rating_raw = product.select_one("span.bold")
+        rating = rating_raw.text if rating_raw else "0"
+
+        review_count_raw = product.select_one("span.text-underline").get_text(strip=True)
+        review_count = review_count_raw if review_count_raw else "0"
+
         products.append({
             "Name": name,
             "Rating": rating,
@@ -25,12 +34,11 @@ def convert_to_df(soup):
             })
         
 
-
     df = pd.DataFrame(products)
 
-    return df
+    df["Rating"] = df["Rating"].fillna("0")
 
-url = "https://www.datart.cz/iphone.html"
+    return df
 
 response = fetch_web_data(url)
 
